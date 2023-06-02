db0353d1c4d78cb299e0c8825beb4570e1b41ffd
Rebah Özkoç <rebahozkoc@sabanciuniv.edu>
2023-06-03 00:37:28 +0300

    Selected text viewer is added

--- src/extension.ts
+++ src/extension.ts
@@ -6,9 +6,21 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
     const treeDataProvider = new MyTreeDataProvider();
     vscode.window.registerTreeDataProvider('parallelaiView', treeDataProvider);
+
+    vscode.window.onDidChangeTextEditorSelection(() => {
+        treeDataProvider.refresh();
+    });
 }
 
+
 class MyTreeDataProvider implements vscode.TreeDataProvider<MyItem> {
+    private _onDidChangeTreeData: vscode.EventEmitter<MyItem | undefined> = new vscode.EventEmitter<MyItem | undefined>();
+    readonly onDidChangeTreeData: vscode.Event<MyItem | undefined> = this._onDidChangeTreeData.event;
+
+    refresh(): void {
+        this._onDidChangeTreeData.fire(undefined);
+    }
+
     getTreeItem(element: MyItem): vscode.TreeItem {
         return element;
     }
@@ -17,11 +29,17 @@ class MyTreeDataProvider implements vscode.TreeDataProvider<MyItem> {
         if (element) {
             return Promise.resolve([]);
         } else {
-            const item = new MyItem("Hello, ParallelAI!");
-            return Promise.resolve([item]);
+            const selectedText = getSelectedText();
+            if (selectedText) {
+                const item = new MyItem(selectedText);
+                return Promise.resolve([item]);
+            } else {
+                return Promise.resolve([]);
+            }
         }
     }
 }
+
 
 class MyItem extends vscode.TreeItem {
     constructor(label: string) {
@@ -29,6 +47,15 @@ class MyItem extends vscode.TreeItem {
     }
 }
 
+function getSelectedText(): string | undefined {
+    const editor = vscode.window.activeTextEditor;
+    if (editor) {
+        const document = editor.document;
+        const selection = editor.selection;
+        return document.getText(selection);
+    }
+    return undefined;
+}
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
