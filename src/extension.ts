9ebc2a747e18636247b3cc8fcc0c48ac78d10309
Rebah Özkoç <rebahozkoc@sabanciuniv.edu>
2023-06-03 00:29:23 +0300

    Added activity bar button

--- src/extension.ts
+++ src/extension.ts
@@ -2,25 +2,34 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+
 export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "parallelai" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('parallelai.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ParallelAI!');
-	});
-
-	context.subscriptions.push(disposable);
+    const treeDataProvider = new MyTreeDataProvider();
+    vscode.window.registerTreeDataProvider('parallelaiView', treeDataProvider);
 }
+
+class MyTreeDataProvider implements vscode.TreeDataProvider<MyItem> {
+    getTreeItem(element: MyItem): vscode.TreeItem {
+        return element;
+    }
+
+    getChildren(element?: MyItem): vscode.ProviderResult<MyItem[]> {
+        if (element) {
+            return Promise.resolve([]);
+        } else {
+            const item = new MyItem("Hello, ParallelAI!");
+            return Promise.resolve([item]);
+        }
+    }
+}
+
+class MyItem extends vscode.TreeItem {
+    constructor(label: string) {
+        super(label);
+    }
+}
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
