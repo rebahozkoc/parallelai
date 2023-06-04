28aa4fc7eb794440e6585fcd75ba6e29b114d2fb
Rebah Özkoç <rebahozkoc@sabanciuniv.edu>
2023-06-04 19:32:24 +0300

    styling the ui

--- src/extension.ts
+++ src/extension.ts
@@ -5,26 +5,26 @@ import { processMessage } from './messageProcessor';
 
 
 export function activate(context: vscode.ExtensionContext) {
-    context.subscriptions.push(vscode.window.registerWebviewViewProvider("parallelaiView", new SelectedTextWebviewViewProvider(context.extensionUri, context)));
+	context.subscriptions.push(vscode.window.registerWebviewViewProvider("parallelaiView", new SelectedTextWebviewViewProvider(context.extensionUri, context)));
 	openaiApiKey = context.globalState.get<string>('openaiApiKey');
-    
-    if (!openaiApiKey) {
-        vscode.window.showInputBox({ prompt: 'Enter your OpenAI API Key' }).then(value => {
-            if (value) {
-                openaiApiKey = value;
-                context.globalState.update('openaiApiKey', value);
-            }
-        });
-    }
-	let disposable = vscode.commands.registerCommand('extension.askChatGPT', function () {
-        const selectedText = getSelectedText();
-        if (selectedText) {
-            console.log(`Asked to chat GPT with text command: ${selectedText}`);
-        }
-    });
 
-    context.subscriptions.push(disposable);
-	
+	if (!openaiApiKey) {
+		vscode.window.showInputBox({ prompt: 'Enter your OpenAI API Key' }).then(value => {
+			if (value) {
+				openaiApiKey = value;
+				context.globalState.update('openaiApiKey', value);
+			}
+		});
+	}
+	let disposable = vscode.commands.registerCommand('extension.askChatGPT', function () {
+		const selectedText = getSelectedText();
+		if (selectedText) {
+			console.log(`Asked to chat GPT with text command: ${selectedText}`);
+		}
+	});
+
+	context.subscriptions.push(disposable);
+
 }
 let openaiApiKey: string | undefined = "";
 let selectedText = "";
@@ -32,19 +32,19 @@ let currentViewProvider: SelectedTextWebviewViewProvider | null = null;
 
 class SelectedTextWebviewViewProvider implements vscode.WebviewViewProvider {
 
-    public view?: vscode.WebviewView;
+	public view?: vscode.WebviewView;
 
-    constructor(private readonly _extensionUri: vscode.Uri, private _context: vscode.ExtensionContext) {
-        this._context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(() => {
-            if (this.view) {
-                this.view.webview.html = this.getHtmlForWebview(this.view.webview);
-            }
-        }));
-    }
+	constructor(private readonly _extensionUri: vscode.Uri, private _context: vscode.ExtensionContext) {
+		this._context.subscriptions.push(vscode.window.onDidChangeTextEditorSelection(() => {
+			if (this.view) {
+				this.view.webview.html = this.getHtmlForWebview(this.view.webview);
+			}
+		}));
+	}
 
 	public resolveWebviewView(
-		webviewView: vscode.WebviewView, 
-		context: vscode.WebviewViewResolveContext, 
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
 		token: vscode.CancellationToken
 	) {
 		this.view = webviewView;
@@ -53,8 +53,8 @@ class SelectedTextWebviewViewProvider implements vscode.WebviewViewProvider {
 		webviewView.webview.options = {
 			enableScripts: true
 		};
-	
-		
+
+
 		webviewView.webview.onDidReceiveMessage(
 			async message => {
 				switch (message.command) {
@@ -63,67 +63,81 @@ class SelectedTextWebviewViewProvider implements vscode.WebviewViewProvider {
 						console.log(`Asked to chat GPT with text: ${selectedText}`);
 						const processedMessage = await processMessage(selectedText, openaiApiKey); // processMessage is your function that processes the message
 						webviewView.webview.postMessage({ command: 'display', text: processedMessage });
-		
+
 						return;
 				}
 			},
 			undefined,
 			this._context.subscriptions
 		);
-	
+
 		this.refresh();
 	}
 
 	public refresh() {
-		if(this.view) {
+		if (this.view) {
 			this.view.webview.html = this.getHtmlForWebview(this.view.webview);
 		}
 	}
 
 	public getHtmlForWebview(webview: vscode.Webview): string {
 		const nonce = getNonce();
-	
+
 		let selectedText = getSelectedText() || 'No selection';
 		selectedText = this._escapeHtml(selectedText);
-	
+
 		return `<!DOCTYPE html>
 			<html lang="en">
 			<head>
-				<meta charset="UTF-8">
-				<meta http-equiv="X-UA-Compatible" content="IE=edge">
-				<meta name="viewport" content="width=device-width, initial-scale=1.0">
-				<title>Selected Text</title>
-				<style>
-					h3 {
-						margin: 10px;
-					}
-					pre {
-						background-color: white;
-						border: 1px solid black;
-						padding: 10px;
-						margin: 10px;
-						white-space: pre-wrap;
-					}
-					button {
-						margin: 10px;
-					}
-				</style>
-			</head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Selected Text</title>
+            <style>
+                h4 {
+                    margin: 5px;
+                }
+                pre {
+                    background-color: white;
+                    border: 0.5px solid black;
+                    padding: 5px;
+                    margin: 5px;
+                    white-space: pre-wrap;
+                }
+                button {
+                    margin: 5px;
+                    background-color: green;
+                    color: white;
+                    border: none;
+                    padding: 5px 15px;
+                    text-align: center;
+                    text-decoration: none;
+                    display: inline-block;
+                    font-size: 12px;
+                    cursor: pointer;
+                }
+            </style>
+        </head>
 			<body>
-			<h3>Your Code:</h3>
+			<h4>Selected Code:</h4>
 			<pre>${selectedText}</pre>
 			<button id="askButton">Ask to ChatGPT</button>
 			<p id="loadingIndicator" style="display: none;">Loading...</p>
-			<h3>Processed Message:</h3>
-			<pre id="processedMessage"></pre>
+			<div id="messageContainer" style="display: none;">
+				<h4>Suggestion:</h4>
+				<pre id="processedMessage"></pre>
+			</div>
 			<script nonce="${nonce}">
 			window.onload = function() {
 				const vscode = acquireVsCodeApi();
 				const loadingIndicator = document.getElementById('loadingIndicator');
+				const messageContainer = document.getElementById('messageContainer');
 
 				document.getElementById('askButton').addEventListener('click', () => {
-					// Show the loading indicator
+					// Show the loading indicator and hide the messageContainer
 					loadingIndicator.style.display = 'block';
+					loadingIndicator.style.margin = '5px';
+					messageContainer.style.display = 'none';
 
 					vscode.postMessage({
 						command: 'askChatGPT',
@@ -138,19 +152,20 @@ class SelectedTextWebviewViewProvider implements vscode.WebviewViewProvider {
 					case 'display':
 						const processedMessage = message.text;
 
-						// Hide the loading indicator
+						// Hide the loading indicator and show the messageContainer
 						loadingIndicator.style.display = 'none';
+						messageContainer.style.display = 'block';
 
 						document.getElementById('processedMessage').textContent = processedMessage;
 						break;
 				}
 			});
-			</script>
+		</script>
 		</body>
 			</html>`;
 	}
-	
-	
+
+
 	private _escapeHtml(html: string): string {
 		return html.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;");
 	}
@@ -160,24 +175,24 @@ class SelectedTextWebviewViewProvider implements vscode.WebviewViewProvider {
 
 
 function getNonce() {
-    let text = '';
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    for (let i = 0; i < 32; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
 }
 
 function getSelectedText(): string | undefined {
-    const editor = vscode.window.activeTextEditor;
-    if (editor) {
-        const document = editor.document;
-        const selection = editor.selection;
+	const editor = vscode.window.activeTextEditor;
+	if (editor) {
+		const document = editor.document;
+		const selection = editor.selection;
 		selectedText = document.getText(selection);
-        return document.getText(selection);
-    }
-    return undefined;
+		return document.getText(selection);
+	}
+	return undefined;
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
